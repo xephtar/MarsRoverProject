5d24e24838b2e2a213707f04b4594b1fb3fa9eab
Ömer Faruk Davarcı <omer.davarci@sabancidx.com>
2021-03-14 04:09:22 +0300

    Implement program

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace MarsRover
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Any())
+            {
+                var path = args[0];
+                if (File.Exists(path))
+                {
+                    TextReader input = File.OpenText(path);
+                    int lineCounter = 0;
+                    Plateau PlateauOnMars = new Plateau();
+                    for (string line; (line = input.ReadLine()) != null; lineCounter++)
+                    {
+                        if (lineCounter == 0)
+                        {
+                            string[] variables = line.Split(' ');
+                            int XMax = int.Parse(variables[0]);
+                            int YMax = int.Parse(variables[1]);
+                            PlateauOnMars = new Plateau(XMax, YMax);
+                            continue;
+                        }
+
+                        if (IsEven(lineCounter))
+                        {
+                            int RoverID = PlateauOnMars.RoverCount - 1;
+                            PlateauOnMars.MoveRoverWithIDAndSequence(RoverID, line);
+                        }
+                        else
+                        {
+                            string[] variables = line.Split(' ');
+                            int XPos = int.Parse(variables[0]);
+                            int YPos = int.Parse(variables[1]);
+                            string Direction = variables[2];
+                            PlateauOnMars.AddRoverWithInformation(XPos, YPos, Direction);
+                        }
+                    }
+                    PlateauOnMars.GetInformationOfRovers();
+                }
+            }
+            else
+            {
+                Console.WriteLine("You should give an input txt file as argument!");
+            }
+        }
+
+        public static bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
